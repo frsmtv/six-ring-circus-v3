0dc176635775a3b7536175a6960c9c12177113d1
frsmtv <fsimitchiev@gmail.com>
2017-01-25 11:41:05 +0100

    site done, todo responsive

--- js/main.js
+++ js/main.js
@@ -5,57 +5,56 @@ $(document).ready(function(){
     $('.animate').scrolla();
 
     // **** UI ANIMATIONS ****
-    $('h1').delay(5000).show('fade', 1000);
-    $('nav').delay(5000).show('fade', 1000);
+    $('#intro').delay(2500).show('fade', 1000);
+    $('nav').delay(2500).show('fade', 1000);
 
     // **** NAVBAR REDUCE ON SCROLL ****
-    var scroll_start = 0;
-    var startchange = $('body');
-    var offset = startchange.offset();
-    $(document).scroll(function() {
-        scroll_start = $(this).scrollTop();
-        if(scroll_start > offset.top) {
-            $('nav').css({
-                height: '25px',
-                'font-size': '0.6rem'
-            });
-        } else {
-            $('nav').css({
-                height: '',
-                'font-size': ''
-            });
-        }
-    });
+    // var scroll_start = 0;
+    // var startchange = $('body');
+    // var offset = startchange.offset();
+    // $(document).scroll(function() {
+    //     scroll_start = $(this).scrollTop();
+    //     if(scroll_start > offset.top) {
+    //         $('nav').css({
+    //             height: '25px',
+    //             'font-size': '0.6rem'
+    //         });
+    //     } else {
+    //         $('nav').css({
+    //             height: '',
+    //             'font-size': ''
+    //         });
+    //     }
+    // });
 
-    // **** NAVBAR ZOOM ON HOVER ****
 
 
     // **** HOME BUTTON ****
     $('#home-btn').click(function(){
         $('html, body').animate({
             scrollTop: $('body').offset().top - 20
-        }, 2000);
+        }, 1000);
     });
 
     // **** INFO SECTION ****
     $('#info-btn').click(function(){
         $('html, body').animate({
             scrollTop: $('#info').offset().top - 20
-        }, 2000);
+        }, 1000);
     });
 
     // **** CONCERTS SECTION ****
     $('#concerts-btn').click(function(){
         $('html, body').animate({
             scrollTop: $('#concerts').offset().top - 20
-        }, 2000);
+        }, 1000);
     });
 
     // **** MEDIAS SECTION ****
     $('#media-btn').click(function(){
         $('html, body').animate({
             scrollTop: $('#video').offset().top - 20
-        }, 2000);
+        }, 1000);
     });
 
 
@@ -63,7 +62,7 @@ $(document).ready(function(){
     $('#contact-btn').click(function(){
         $('html, body').animate({
             scrollTop: $('#contact').offset().top - 20
-        }, 2000);
+        }, 1000);
     });
 
 
